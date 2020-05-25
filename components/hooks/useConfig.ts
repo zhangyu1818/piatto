db0b9e1c1d996ba73a80720276ed8a156b0b6ccb
zhangyu1818 <723156735@qq.com>
2020-05-25 10:55:20 +0800

    feat: useConfig hook

---
+++ components/hooks/useConfig.ts
@@ -0,0 +1,9 @@
+import { useContext } from 'react';
+import { ConfigContext } from '../config-provider';
+
+const useConfig = () => {
+  const context = useContext(ConfigContext);
+  return context;
+};
+
+export default useConfig;
