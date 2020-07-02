5f4b91deb1fbe52492efeb49389e8d9a22195fb5
zhangyu1818 <723156735@qq.com>
2020-07-02 14:33:39 +0800

    feat: add ConfigProvider component

--- components/config-provider/context.ts
+++ components/config-provider/context.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 
-interface ConfigContextProps {
+export interface ConfigContextProps {
   getPrefixCls(suffixCls: string, customClass?: string): string;
 }
 
--- components/config-provider/index.tsx
+++ components/config-provider/index.tsx
@@ -1,3 +1,14 @@
-import ConfigContext from './context';
+import React from 'react';
+import ConfigContext, { ConfigContextProps } from './context';
+
+export interface ConfigProviderProps {
+  value: ConfigContextProps;
+}
+
+const ConfigProvider: React.FC<ConfigProviderProps> = ({ children, value }) => {
+  return <ConfigContext.Provider value={value}>{children}</ConfigContext.Provider>;
+};
+
+export default ConfigProvider;
 
 export { ConfigContext };
