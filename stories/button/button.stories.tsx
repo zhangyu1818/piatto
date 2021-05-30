56d2bb413b28ba8115bd0a0f94c820f0dc80ff3e
zhangyu1818 <723156735@qq.com>
2021-05-30 15:42:15 +0800

    update button stories

--- stories/button/button.stories.tsx
+++ stories/button/button.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button } from 'piatto'
+import { Button, Space } from 'piatto'
 
 import type { Meta, Story } from '@storybook/react'
 import type { ButtonProps } from 'piatto/button'
@@ -29,5 +29,36 @@ export default {
   },
 } as Meta
 
-export const Basic: Story<ButtonProps> = (args) => <Button {...args}>Primary Button</Button>
+export const Basic: Story<ButtonProps> = (args) => (
+  <Space direction="vertical">
+    <Button {...args}>Button</Button>
+    <Button {...args} type="default">
+      Default Button
+    </Button>
+    <Button {...args} type="primary">
+      Primary Button
+    </Button>
+    <Button {...args} type="link">
+      Link Button
+    </Button>
+    <Button {...args} type="default" loading>
+      Loading Button
+    </Button>
+    <Button {...args} type="primary" loading>
+      Loading Button
+    </Button>
+    <Button {...args} type="link" loading>
+      Loading Button
+    </Button>
+    <Button {...args} block>
+      Block Button
+    </Button>
+    <Button {...args} shape="round">
+      Round Button
+    </Button>
+    <Button {...args} block shape="round">
+      Block Round Button
+    </Button>
+  </Space>
+)
 Basic.storyName = 'Button'
